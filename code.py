b529f9611f349f350eccbbacde39d717b348271c
egorik3300 <83121236+egorik3300@users.noreply.github.com>
2021-05-08 15:31:49 +0300

    Update code.py
    covid 19 GAME

--- code.py
+++ code.py
@@ -2,8 +2,16 @@ from pygame import*
 w=1365
 h=767
 win = display.set_mode((w,h))
+class Alfa(sprite.Sprite):
+    def __init__(self,x,y,h,w,image_name,speed)
+        super().__init__()
+        self.image = image.load(image_name)
+        self.rect = Rect(w,h,y,x)
+    def reset(self):
+        win.blit(self.image,(self.rect.x,self.rect.y))
+ball = Alfa(0,0,100,100,'мяч.jpg',0)
+
 while True:
-   
     for e in event.get():
       if e.type==12:
           exit()
